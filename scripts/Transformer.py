7704bf26c826812eb159211dfa43374b62b9e1dd
YiwenZheng <zyw119988@163.com>
2021-03-26 09:41:45 +0800

    Transformer

--- scripts/Transformer.py
+++ scripts/Transformer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-from torch import nn
-from model_helper import Embeddings, Encoder_MultipleLayers
-from utils import drug2emb_encoder, generate_config
+from utils import generate_config
 from argparse import ArgumentParser
-from encoders import Transformer
 from train_CompoundPred import train_model
-
 
 
 def get_parser():
@@ -18,26 +14,30 @@ def get_parser():
         help = "Directory where model will be saved")
     parser.add_argument("-d_e", "--drug_encoding", type = str, default = "Transformer",\
         required = False, help = "Drug encoding")
-    parser.add_argument("--result_folder", type = str, default = "./result/", required = False,\
-        help = "Folder of result") 
-    parser.add_argument("--input_dim_drug", type = int, default = 1024, required = False,\
-        help = "Dimensionality of input of drug") 
-    parser.add_argument("--input_dim_protein", type = int, default = 8420, required = False,\
-        help = "Dimensionality of input of protein") 
-    parser.add_argument("--hidden_dim_drug", type = int, default = 256, required = False,\
-        help = "Dimensionality of hidden layers of drug") 
-    parser.add_argument("--hidden_dim_protein", type = int, default = 256, required = False,\
-        help = "Dimensionality of hidden layers of protein") 
-    parser.add_argument("--cls_hidden_dims", type = list, default = [1024, 1024, 512], required = False,\
-        help = "Dimensionality of hidden layers of drug in CLS") 
-    parser.add_argument("--batch_size", type = int, default = 256, required = False, help = "Batch size") 
+    parser.add_argument("--result_folder", type = str, default = "./result/",\
+        required = False, help = "Folder of result") 
+    parser.add_argument("--input_dim_drug", type = int, default = 1024,\
+        required = False,help = "Dimensionality of input of drug") 
+    parser.add_argument("--input_dim_protein", type = int, default = 8420,\
+        required = False, help = "Dimensionality of input of protein") 
+    parser.add_argument("--hidden_dim_drug", type = int, default = 256,\
+        required = False, help = "Dimensionality of hidden layers of drug") 
+    parser.add_argument("--hidden_dim_protein", type = int, default = 256,\
+        required = False, help = "Dimensionality of hidden layers of protein") 
+    parser.add_argument("--cls_hidden_dims", type = list, default = [1024, 1024, 512],\
+        required = False, help = "Dimensionality of hidden layers of drug in CLS") 
+    parser.add_argument("--batch_size", type = int, default = 256, required = False,\
+        help = "Batch size") 
     parser.add_argument("--train_epoch", type = int, default = 10, required = False,\
         help = "Number of epochs during training") 
-    parser.add_argument("--test_every_X_epoch", type = int, default = 20, required = False,\
-        help = "Number of epochs during testing") 
-    parser.add_argument("--LR", type = int, default = 1e-4, required = False, help = "Learing rate") 
-    parser.add_argument("--decay", type = int, default = 0, required = False, help = "Decay") 
-    parser.add_argument("--num_workers", type = int, default = 0, required = False, help = "Number of workers") 
+    parser.add_argument("--test_every_X_epoch", type = int, default = 20,\
+        required = False, help = "Number of epochs during testing") 
+    parser.add_argument("--LR", type = int, default = 1e-4, required = False,\
+        help = "Learing rate") 
+    parser.add_argument("--decay", type = int, default = 0, required = False,\
+        help = "Decay") 
+    parser.add_argument("--num_workers", type = int, default = 0, required = False,\
+        help = "Number of workers") 
 
     # config of drug
     parser.add_argument("--transformer_dropout_rate", type = int, default = 0.1,\
@@ -56,43 +56,44 @@ def get_parser():
         required = False, help = "Dropout rate of hidden layers in Transformer")
 
     # config of target
-    parser.add_argument("--transformer_emb_size_target", type = int, default = 64, required = False,\
-        help = "Size of embedding of target in Transformer") 
+    parser.add_argument("--transformer_emb_size_target", type = int, default = 64,\
+        required = False, help = "Size of embedding of target in Transformer") 
     parser.add_argument("--transformer_intermediate_size_target", type = int, default = 256,\
         required = False, help = "Size of intermediate layers of target in Transformer") 
     parser.add_argument("--transformer_num_attention_heads_target", type = int, default = 4,\
         required = False, help = "Number of heads of attention layers of target in Transformer") 
-    parser.add_argument("--transformer_n_layer_target", type = int, default = 2, required = False,\
-        help = "Number of layers of target in Transfromer") 
+    parser.add_argument("--transformer_n_layer_target", type = int, default = 2,\
+        required = False, help = "Number of layers of target in Transfromer") 
 
     return parser
 
+
 #模型配置生成
-def this_model_config(config):
+def get_model_config(config):
     model_config = generate_config(drug_encoding = config.drug_encoding, 
-                        result_folder = config.result_folder,
-                        input_dim_drug = config.input_dim_drug, 
-                        input_dim_protein = config.input_dim_protein,
-                        hidden_dim_drug = config.hidden_dim_drug, 
-                        hidden_dim_protein = config.hidden_dim_protein,
-                        cls_hidden_dims = config.cls_hidden_dims,
-                        batch_size = config.batch_size,
-                        train_epoch = config.train_epoch,
-                        test_every_X_epoch = config.test_every_X_epoch,
-                        LR = config.LR,
-                        decay = config.decay,
-                        transformer_emb_size_drug = config.transformer_emb_size_drug,
-                        transformer_intermediate_size_drug = config.transformer_intermediate_size_drug,
-                        transformer_num_attention_heads_drug = config.transformer_num_attention_heads_drug,
-                        transformer_n_layer_drug = config.transformer_n_layer_drug,
-                        transformer_emb_size_target = config.transformer_emb_size_target,
-                        transformer_intermediate_size_target = config.transformer_intermediate_size_target,
-                        transformer_num_attention_heads_target = config.transformer_num_attention_heads_target,
-                        transformer_n_layer_target = config.transformer_n_layer_target,
-                        transformer_dropout_rate = config.transformer_dropout_rate,
-                        transformer_attention_probs_dropout = config.transformer_attention_probs_dropout,
-                        transformer_hidden_dropout_rate = config.transformer_hidden_dropout_rate,
-                        num_workers = config.num_workers)
+            result_folder = config.result_folder,
+            input_dim_drug = config.input_dim_drug, 
+            input_dim_protein = config.input_dim_protein,
+            hidden_dim_drug = config.hidden_dim_drug, 
+            hidden_dim_protein = config.hidden_dim_protein,
+            cls_hidden_dims = config.cls_hidden_dims,
+            batch_size = config.batch_size,
+            train_epoch = config.train_epoch,
+            test_every_X_epoch = config.test_every_X_epoch,
+            LR = config.LR,
+            decay = config.decay,
+            transformer_emb_size_drug = config.transformer_emb_size_drug,
+            transformer_intermediate_size_drug = config.transformer_intermediate_size_drug,
+            transformer_num_attention_heads_drug = config.transformer_num_attention_heads_drug,
+            transformer_n_layer_drug = config.transformer_n_layer_drug,
+            transformer_emb_size_target = config.transformer_emb_size_target,
+            transformer_intermediate_size_target = config.transformer_intermediate_size_target,
+            transformer_num_attention_heads_target = config.transformer_num_attention_heads_target,
+            transformer_n_layer_target = config.transformer_n_layer_target,
+            transformer_dropout_rate = config.transformer_dropout_rate,
+            transformer_attention_probs_dropout = config.transformer_attention_probs_dropout,
+            transformer_hidden_dropout_rate = config.transformer_hidden_dropout_rate,
+            num_workers = config.num_workers)
     return model_config
 
 if __name__ == "__main__":
