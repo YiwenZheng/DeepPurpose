850889f099dee7d8d449fd52f4bdf2d65a64c91b
YiwenZheng <zyw119988@163.com>
2021-03-30 15:30:24 +0800

    update

--- scripts/Trans_hyperopt.py
+++ scripts/Trans_hyperopt.py
@@ -7,14 +7,14 @@ from argparse import ArgumentParser
 
 def objective(args):
     parser = ArgumentParser()
-    parser.add_argument("-i", "--input_file", type = str, required = True,\
+    parser.add_argument("-i", "--input_file", type = str, required = True,
         help = "Path to data file")
-    parser.add_argument("-o", "--output_dir", type = str, required = True,\
+    parser.add_argument("-o", "--output_dir", type = str, required = True,
         help = "Directory where model will be saved")
-    parser.add_argument("-r", "--result_folder", type = str, default = "./result/",\
-        required = False, help = "Folder of result") 
-    parser.add_argument("-d_e", "--drug_encoding", type = str, default = "Transformer",\
-        required = False, help = "Drug encoding")
+    parser.add_argument("-r", "--result_folder", type = str, 
+        default = "./result/", required = False, help = "Folder of result") 
+    parser.add_argument("-d_e", "--drug_encoding", type = str, 
+        default = "Transformer", required = False, help = "Drug encoding")
     tmp = parser.parse_args()
     base_config = vars(tmp)  #把参数转换为字典格式
 
@@ -37,14 +37,20 @@ def objective(args):
         "transformer_dropout_rate": args["transformer_dropout_rate"],
         "transformer_emb_size_drug": args["transformer_emb_size_drug"],
         "transformer_n_layer_drug": args["transformer_n_layer_drug"],
-        "transformer_intermediate_size_drug": args["transformer_intermediate_size_drug"],
-        "transformer_num_attention_heads_drug": args["transformer_num_attention_heads_drug"],
-        "transformer_attention_probs_dropout": args["transformer_attention_probs_dropout"],
-        "transformer_hidden_dropout_rate": args["transformer_hidden_dropout_rate"],
+        "transformer_intermediate_size_drug": 
+            args["transformer_intermediate_size_drug"],
+        "transformer_num_attention_heads_drug": 
+            args["transformer_num_attention_heads_drug"],
+        "transformer_attention_probs_dropout": 
+            args["transformer_attention_probs_dropout"],
+        "transformer_hidden_dropout_rate": 
+            args["transformer_hidden_dropout_rate"],
         "transformer_emb_size_target": args["transformer_emb_size_target"],
         "transformer_n_layer_target": args["transformer_n_layer_target"],
-        "transformer_intermediate_size_target": args["transformer_intermediate_size_target"],
-        "transformer_num_attention_heads_target": args["transformer_num_attention_heads_target"]
+        "transformer_intermediate_size_target": 
+            args["transformer_intermediate_size_target"],
+        "transformer_num_attention_heads_target": 
+            args["transformer_num_attention_heads_target"]
     }
     test_result = train_model(params)
     return 1-test_result["AUROC"]
@@ -56,16 +62,24 @@ space = {
     "hidden_dim_protein": hp.choice("hidden_dim_protein", [128, 256, 512]),
     "batch_size": hp.choice("batch_size", [32, 64, 128, 256, 512]),
     "transformer_dropout_rate": hp.choice("transformer_dropout_rate", [0.1]),
-    "transformer_emb_size_drug": hp.choice("transformer_emb_size_drug", [128, 256]),
-    "transformer_n_layer_drug": hp.choice("transformer_n_layer_drug", range(1,10)),
-    "transformer_intermediate_size_drug": hp.choice("transformer_intermediate_size_drug", [128, 256, 512]),
-    "transformer_num_attention_heads_drug": hp.choice("transformer_num_attention_heads_drug", [2, 4, 8]),
-    "transformer_attention_probs_dropout": hp.choice("transformer_attention_probs_dropout", [0.1]),
-    "transformer_hidden_dropout_rate": hp.choice("transformer_hidden_dropout_rate", [0.1]),
-    "transformer_emb_size_target": hp.choice("transformer_emb_size_target", [32, 64, 128]),
-    "transformer_n_layer_target": hp.choice("transformer_n_layer_target", range(1,10)),
-    "transformer_intermediate_size_target": hp.choice("transformer_intermediate_size_target", [128, 256, 512]),
-    "transformer_num_attention_heads_target": hp.choice("transformer_num_attention_heads_target", [2, 4, 8])
+    "transformer_emb_size_drug":hp.choice("transformer_emb_size_drug",[128,256]),
+    "transformer_n_layer_drug":hp.choice("transformer_n_layer_drug",range(1,10)),
+    "transformer_intermediate_size_drug": 
+        hp.choice("transformer_intermediate_size_drug", [128, 256, 512]),
+    "transformer_num_attention_heads_drug": 
+        hp.choice("transformer_num_attention_heads_drug", [2, 4, 8]),
+    "transformer_attention_probs_dropout": 
+        hp.choice("transformer_attention_probs_dropout", [0.1]),
+    "transformer_hidden_dropout_rate": 
+        hp.choice("transformer_hidden_dropout_rate", [0.1]),
+    "transformer_emb_size_target": 
+        hp.choice("transformer_emb_size_target", [32, 64, 128]),
+    "transformer_n_layer_target": 
+        hp.choice("transformer_n_layer_target", range(1,10)),
+    "transformer_intermediate_size_target": 
+        hp.choice("transformer_intermediate_size_target", [128, 256, 512]),
+    "transformer_num_attention_heads_target": 
+        hp.choice("transformer_num_attention_heads_target", [2, 4, 8])
 }
 
 best = fmin(fn = objective, space = space, algo = tpe.suggest, max_evals = 100)
--- scripts/Transformer.py
+++ scripts/Transformer.py
@@ -8,61 +8,69 @@ from train_CompoundPred import train_model
 def get_parser():
     parser = ArgumentParser()
     # base config
-    parser.add_argument("-i", "--input_file", type = str, required = True,\
+    parser.add_argument("-i", "--input_file", type = str, required = True,
         help = "Path to data file")
-    parser.add_argument("-o", "--output_dir", type = str, required = True,\
+    parser.add_argument("-o", "--output_dir", type = str, required = True,
         help = "Directory where model will be saved")
-    parser.add_argument("-r", "--result_folder", type = str, default = "./result/",\
-        required = False, help = "Folder of result") 
-    parser.add_argument("-d_e", "--drug_encoding", type = str, default = "Transformer",\
-        required = False, help = "Drug encoding")
-    parser.add_argument("--input_dim_drug", type = int, default = 2586,\
+    parser.add_argument("-r", "--result_folder", type = str, 
+        default = "./result/", required = False, help = "Folder of result") 
+    parser.add_argument("-d_e", "--drug_encoding", type = str, 
+        default = "Transformer", required = False, help = "Drug encoding")
+    parser.add_argument("--input_dim_drug", type = int, default = 2586,
         required = False,help = "Dimensionality of input of drug") 
-    parser.add_argument("--input_dim_protein", type = int, default = 4114,\
+    parser.add_argument("--input_dim_protein", type = int, default = 4114,
         required = False, help = "Dimensionality of input of protein") 
-    parser.add_argument("--hidden_dim_drug", type = int, default = 256,\
+    parser.add_argument("--hidden_dim_drug", type = int, default = 256,
         required = False, help = "Dimensionality of hidden layers of drug") 
-    parser.add_argument("--hidden_dim_protein", type = int, default = 256,\
+    parser.add_argument("--hidden_dim_protein", type = int, default = 256,
         required = False, help = "Dimensionality of hidden layers of protein") 
-    parser.add_argument("--cls_hidden_dims", type = list, default = [1024, 1024, 512],\
-        required = False, help = "Dimensionality of hidden layers of decoder classifier") 
-    parser.add_argument("--batch_size", type = int, default = 256, required = False,\
-        help = "Batch size") 
-    parser.add_argument("--train_epoch", type = int, default = 10, required = False,\
-        help = "Number of epochs during training") 
-    parser.add_argument("--test_every_X_epoch", type = int, default = 20,\
+    parser.add_argument("--cls_hidden_dims", type = list, 
+        default = [1024, 1024, 512], required = False, 
+        help = "Dimensionality of hidden layers of decoder classifier") 
+    parser.add_argument("--batch_size", type = int, default = 256, 
+        required = False, help = "Batch size") 
+    parser.add_argument("--train_epoch", type = int, default = 10, 
+        required = False, help = "Number of epochs during training") 
+    parser.add_argument("--test_every_X_epoch", type = int, default = 20,
         required = False, help = "Number of epochs during testing") 
-    parser.add_argument("--LR", type = int, default = 1e-4, required = False,\
+    parser.add_argument("--LR", type = int, default = 1e-4, required = False,
         help = "Learing rate") 
-    parser.add_argument("--decay", type = int, default = 0, required = False,\
+    parser.add_argument("--decay", type = int, default = 0, required = False,
         help = "Decay") 
-    parser.add_argument("--num_workers", type = int, default = 0, required = False,\
-        help = "Number of workers") 
+    parser.add_argument("--num_workers", type = int, default = 0, 
+        required = False, help = "Number of workers") 
 
     # config of drug
-    parser.add_argument("--transformer_dropout_rate", type = int, default = 0.1,\
+    parser.add_argument("--transformer_dropout_rate", type = int, default = 0.1,
         required = False, help = "Dropout tate in Transformer") 
-    parser.add_argument("--transformer_emb_size_drug", type = int, default = 128,\
+    parser.add_argument("--transformer_emb_size_drug", type = int, default = 128,
         required = False, help = "Size of embedding of drug in Transformer") 
-    parser.add_argument("--transformer_n_layer_drug", type = int, default = 8,\
+    parser.add_argument("--transformer_n_layer_drug", type = int, default = 8,
         required = False, help = "Number of layers of drug in Transfromer") 
-    parser.add_argument("--transformer_intermediate_size_drug", type = int, default = 512,\
-        required = False, help = "Size of intermediate layers of drug in Transformer") 
-    parser.add_argument("--transformer_num_attention_heads_drug", type = int, default = 8,\
-        required = False, help = "Number of heads of attention layers of drug in Transformer")
-    parser.add_argument("--transformer_attention_probs_dropout", type = int, default = 0.1,\
-        required = False, help = "Dropout probability of attention layers in Transformer") 
-    parser.add_argument("--transformer_hidden_dropout_rate", type = int, default = 0.1,\
-        required = False, help = "Dropout rate of hidden layers in Transformer")
+    parser.add_argument("--transformer_intermediate_size_drug", 
+        type = int, default = 512, required = False, 
+        help = "Size of intermediate layers of drug in Transformer") 
+    parser.add_argument("--transformer_num_attention_heads_drug", 
+        type = int, default = 8, required = False, 
+        help = "Number of heads of attention layers of drug in Transformer")
+    parser.add_argument("--transformer_attention_probs_dropout", 
+        type = int, default = 0.1, required = False, 
+        help = "Dropout probability of attention layers in Transformer") 
+    parser.add_argument("--transformer_hidden_dropout_rate", 
+        type = int, default = 0.1, required = False, 
+        help = "Dropout rate of hidden layers in Transformer")
 
     # config of target
-    parser.add_argument("--transformer_emb_size_target", type = int, default = 64,\
-        required = False, help = "Size of embedding of target in Transformer") 
-    parser.add_argument("--transformer_intermediate_size_target", type = int, default = 256,\
-        required = False, help = "Size of intermediate layers of target in Transformer") 
-    parser.add_argument("--transformer_num_attention_heads_target", type = int, default = 4,\
-        required = False, help = "Number of heads of attention layers of target in Transformer") 
-    parser.add_argument("--transformer_n_layer_target", type = int, default = 2,\
+    parser.add_argument("--transformer_emb_size_target", 
+        type = int, default = 64, required = False, 
+        help = "Size of embedding of target in Transformer") 
+    parser.add_argument("--transformer_intermediate_size_target", 
+        type = int, default = 256, required = False, 
+        help = "Size of intermediate layers of target in Transformer") 
+    parser.add_argument("--transformer_num_attention_heads_target", 
+        type = int, default = 4, required = False, 
+        help = "Number of heads of attention layers of target in Transformer") 
+    parser.add_argument("--transformer_n_layer_target", type = int, default = 2,
         required = False, help = "Number of layers of target in Transfromer") 
 
     return parser
@@ -84,16 +92,22 @@ def get_model_config(config):
             decay = config["decay"],
             num_workers = config["num_workers"],
             transformer_emb_size_drug = config["transformer_emb_size_drug"],
-            transformer_intermediate_size_drug = config["transformer_intermediate_size_drug"],
-            transformer_num_attention_heads_drug = config["transformer_num_attention_heads_drug"],
+            transformer_intermediate_size_drug = 
+                config["transformer_intermediate_size_drug"],
+            transformer_num_attention_heads_drug = 
+                config["transformer_num_attention_heads_drug"],
             transformer_n_layer_drug = config["transformer_n_layer_drug"],
             transformer_emb_size_target = config["transformer_emb_size_target"],
-            transformer_intermediate_size_target = config["transformer_intermediate_size_target"],
-            transformer_num_attention_heads_target = config["transformer_num_attention_heads_target"],
+            transformer_intermediate_size_target = 
+                config["transformer_intermediate_size_target"],
+            transformer_num_attention_heads_target = 
+                config["transformer_num_attention_heads_target"],
             transformer_n_layer_target = config["transformer_n_layer_target"],
             transformer_dropout_rate = config["transformer_dropout_rate"],
-            transformer_attention_probs_dropout = config["transformer_attention_probs_dropout"],
-            transformer_hidden_dropout_rate = config["transformer_hidden_dropout_rate"])
+            transformer_attention_probs_dropout = 
+                config["transformer_attention_probs_dropout"],
+            transformer_hidden_dropout_rate = 
+                config["transformer_hidden_dropout_rate"])
     return model_config
 
 
